a18a73297964efece36f24f558b9b7a7d217d36c
Christian Donnerer <christian.donnerer@gmail.com>
2020-02-26 21:45:16 +0000

    upsample layer implemented

--- src/UNet.jl
+++ src/UNet.jl
@@ -1,6 +1,34 @@
 module UNet
 
 using Flux
+using Flux: @treelike
+
+struct UpSample
+  ratio
+end
+
+@treelike UpSample
+
+function (up::UpSample)(input)
+  ratio = (up.ratio[1], up.ratio[2], 1, 1)
+  (h, w, c, n) = size(input)
+  y = similar(input, (ratio[1], 1, ratio[2], 1, 1, 1))
+  fill!(y, 1)
+  z = reshape(input, (1, h, 1, w, c, n))  .* y
+  return reshape(z, size(input) .* ratio)
+end
+
+model = Chain(
+    UpSample((1,1))
+)
+
+img = reshape(
+    [1 1 1 1 0 0 0 0 0 0 0 0], 2, 2, 3, 1
+)
+
+conv_img = model(img)
+
+
 
 drop4 = Chain(
     Conv((3,3), 3=>64, relu, pad=(1, 1), stride=(1, 1)),
