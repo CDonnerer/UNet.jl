704a7b42a114b4ec7fd4da2f9d32defedc3e6403
Christian Donnerer <christian.donnerer@gmail.com>
2020-03-03 22:27:27 +0000

    yes it is definitely learning the segmentation

--- src/UNet.jl
+++ src/UNet.jl
@@ -49,7 +49,7 @@ depth_4 = Chain(
     MaxPool((2,2)),
     Conv((3,3), 512=>1024, relu, pad=(0,0), stride=(1,1)),
     Conv((3,3), 1024=>1024, relu, pad=(0,0), stride=(1,1)),
-    Dropout(0.5),
+    #Dropout(0.5),
     UpSample((2,2)),
     Conv((1,1), 1024=>512, pad=(0,0), stride=(1,1)),
 )
@@ -58,7 +58,7 @@ depth_3 = Chain(
     MaxPool((2,2)),
     Conv((3,3), 256=>512, relu, pad=(0,0), stride=(1,1)),
     Conv((3,3), 512=>512, relu, pad=(0,0), stride=(1,1)),
-    Dropout(0.5),
+    #Dropout(0.5),
 
     CroppedSkipConnection(depth_4, +),
     Conv((3,3), 1024=>512, relu, pad=(0,0), stride=(1,1)),
@@ -134,7 +134,16 @@ label = load_img_array("data/membrane/train/label/0.png")
 X_train = img[1:256, 1:256, :, :]
 y_train = label[95:162, 95:162, :, :]
 
-dataset = [(X_train, y_train)]
+a = []
+
+push!(a, (X_train, y_train))
+
+dataset = []
+
+for i = 0:10
+    X, y = get_Xy_train(i)
+    push!(dataset, (X,y))
+end
 
 y_pred = unet(X_train)
 
@@ -142,26 +151,59 @@ loss(x, y) = Flux.crossentropy(
     reshape(unet(x), size(y)[1]^2), reshape(y, size(y)[1]^2),
 )
 
-opt = ADAM()
+opt = ADAM(0.00001 d)
 
 using Flux: throttle, params, crossentropy
 using Juno: @progress
 
-@progress for i = 1:50
-  @info "Epoch $i"
-  Flux.train!(loss, params(unet), dataset, opt)
-end
+tx, ty = dataset[2]
+evalcb = () -> @show loss(tx, ty)
 
+@progress for i = 1:2
+    @info "Epoch $i"
+    Flux.train!(loss, params(unet), dataset, opt, cb = evalcb)
+end
 
 function plot_y(y)
     convert(
         Array{Gray{Normed{UInt8,8}},2},
-        reshape(y.data, 68, 68)
+        reshape(y.data, size(y)[1], size(y)[2])
     )
+end
+
+function plot_y(y::Array{Float32,4})
+    convert(
+        Array{Gray{Normed{UInt8,8}},2},
+        reshape(y, size(y)[1], size(y)[2])
+    )
+end
+
+
+function get_Xy_train(number)
+    img = load_img_array(
+        "data/membrane/train/image/"*string(number)*".png"
+    )
+    label = load_img_array(
+        "data/membrane/train/label/"*string(number)*".png"
+    )
+    return img[1:256, 1:256, :, :], label[95:162, 95:162, :, :]
 end
 
 y_pred = unet(X_train)
 plot_y(y_pred)
+plot_y(y_train)
+plot_y(img[95:162, 95:162, :, :])
+plot_y(y_train)
 
+convert(
+    Array{Gray{Normed{UInt8,8}},2},
+    reshape(y_pred.data, 68, 68)
+)
+
+X, y = get_X_y(1)
+y_pred = unet(X)
+plot_y(X)
+plot_y(y)
+plot_y(y_pred)
 
 end # module
