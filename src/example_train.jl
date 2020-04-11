3b69484fbbf2e44b8a917b33d0faf4e3c6b98064
Christian Donnerer <christian.donnerer@gmail.com>
2020-04-11 18:52:06 +0100

    updated example training script

--- src/example_train.jl
+++ src/example_train.jl
@@ -1,11 +1,8 @@
-# An example of how you can train this UNet and do inference
-using Images
-using FileIO
-using JLD2
+# Example script of how you can train this UNet and do inference
+using Images, FileIO, JLD2, Flux, UNet
 
 using Juno: @progress
 
-using UNet
 
 function check_img_size(img)
     # checks that image size is compatible with the unet
@@ -35,7 +32,7 @@ end
 # build up dataset for training
 dataset = []
 for i = 0:20
-    img, label = load_img_label(i)
+    img, label = load_img_label(i, 340)
     X = img2array(unet_tiling(img))
     y = img2array(label)
     push!(dataset, (X, y))
@@ -44,12 +41,16 @@ end
 loss(x, y) = Flux.crossentropy(
     reshape(unet(x), size(y)[1]^2), reshape(y, size(y)[1]^2),
 )
-opt = ADAM(1e-3)
+opt = Flux.ADAM(1e-4)
 
-tx, ty = dataset[1]
-evalcb = () -> @show loss(tx, ty)
+# get a validation datapoint
+img, label = load_img_label(25, 340)
+X_valid = img2array(unet_tiling(img))
+y_valid = img2array(label)
 
-@progress for i = 0:10
+evalcb = () -> @show loss(X_valid, y_valid)
+
+@progress for i = 0:5
     @info "Epoch $i"
     Flux.train!(loss, params(unet), dataset, opt, cb = evalcb)
 end
@@ -58,12 +59,13 @@ weights = params(unet)
 @save "unet_weights.jld2" weights
 
 # ---------------------------------------------
-# And now, we can do some inference
+# And now, we can do some inference at last
 # ---------------------------------------------
 
 @load "unet_weights.jld2" weights
 Flux.loadparams!(unet, weights)
 
-img, label = load_img_label(4)
+img, label = load_img_label(21)
 y_pred = unet(img2array(unet_tiling(img)))
-array2img(y_pred)
+
+img_pred = array2img(y_pred)
